568640a122c5a9b3947adc52a41a115d7bf06cb5
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2023-04-01 15:19:20 -0500

    Add basic routing and test component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "next/image";
+import { Link, Route, Routes } from "react-router-dom";
+import TestPage from "./pages/TestPage";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav />
       </header>
+      <Routes>
+        <Route path="/TestPage" element={<TestPage />} />
+      </Routes>
     </div>
   );
 }
