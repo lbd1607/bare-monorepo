3a1454b5f003385faf55cb61acec720b93b1ece3
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2023-04-01 16:30:34 -0500

    Set up relay env

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -5,17 +5,21 @@ import Image from "next/image";
 import { Link, Route, Routes } from "react-router-dom";
 import TestPage from "./pages/TestPage";
 import Nav from "./Nav";
+import { RelayEnvironmentProvider, useRelayEnvironment } from "react-relay";
+import environment from "./RelayEnv";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Nav />
-      </header>
-      <Routes>
-        <Route path="/TestPage" element={<TestPage />} />
-      </Routes>
-    </div>
+    <RelayEnvironmentProvider environment={environment}>
+      <div className="App">
+        <header>
+          <Nav />
+        </header>
+        <Routes>
+          <Route path="/TestPage" element={<TestPage />} />
+        </Routes>
+      </div>
+    </RelayEnvironmentProvider>
   );
 }
 
