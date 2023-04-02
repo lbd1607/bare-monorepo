9f95e5577ff1feb521707871122f32cae7ab7919
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2023-04-02 17:21:39 -0500

    Generate dist

--- apps/gql/dist/index.js
+++ apps/gql/dist/index.js
@@ -2,9 +2,9 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema } from "../dist/schema/index.js";
 const server = new ApolloServer({
-  schema,
+    schema,
 });
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 7475 },
+    listen: { port: 7475 },
 });
 console.log(`🚀  Server ready at: ${url}`);
